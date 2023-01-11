33ea2544bc0fc105f76a86d35b44332e3a2f180d
Dashaun Sutton-Harris <114096734+dashaunn@users.noreply.github.com>
2023-01-10 19:55:15 -0600

    completed feature-horoscopes

---
+++ Models/Horoscope.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Astrocosm.Models
+{
+	public class Horoscope
+	{
+		public string Date_Range { get; set; }
+		public string Current_Date { get; set; }
+		public string Description { get; set; }
+		public string Compatibility { get; set; }
+		public string Mood { get; set; }
+		public string Color { get; set; }
+		public string Lucky_Number { get; set; }
+		public string Lucky_Time { get; set; }
+	}
+}
+
